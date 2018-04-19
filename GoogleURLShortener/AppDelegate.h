f68ad365160c85a57c229d827657620e747b4706
Jose <josemsg@gmail.com>
2018-04-19 10:38:47 +0200

    First commit

---
+++ GoogleURLShortener/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoogleURLShortener
+//
+//  Created by Jose on 19/4/18.
+//  Copyright © 2018 Copanonga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
